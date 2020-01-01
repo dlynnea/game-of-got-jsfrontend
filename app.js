d6d848661e7b342feb0f00d9947e27632acbff13
CarlKarlQarl <karlmatthes@gmail.com>
2020-01-01 13:57:23 -0700

    Added framework and page for king deletion via a wedding invitation

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const divCard = document.getElementById("king-card")
+const weddingDropdown = document.getElementById("wedding-dropdown")
+const inviteButton = document.getElementById("invite-button")
 
 fetch("http://localhost:3000/kings")
     .then(response => response.json())
@@ -26,3 +28,25 @@ function deleteKing(id) {
         method:'DELETE'
     })
 }
+
+// Refactor into earlier fetch later
+fetch(`http://localhost:3000/kings`)
+    .then(response => response.json())
+    .then(kings => {
+        fillInWeddingDropdown(kings)
+    })
+    .catch(error => console.log(error))
+
+function fillInWeddingDropdown(kings){
+    kings.map(king => {
+        let kingOption = document.createElement("option")
+        kingOption.innerText = king.name
+        kingOption.value = king.id
+        weddingDropdown.appendChild(kingOption)
+    })
+}
+
+inviteButton.addEventListener("click", () => {
+    kingID = weddingDropdown.value
+    location.href = `http://localhost:3001/wedding.html?id=${kingID}`
+})
---
+++ wedding.js
@@ -0,0 +1,34 @@
+const kingID = new URLSearchParams(window.location.search).get("id")
+const theKingDiv = document.getElementById("king-div")
+const deadDiv = document.getElementById("dead-div")
+const lordlessUL = document.getElementById("lordless-ul")
+
+
+fetch(`http://localhost:3000/kings/${kingID}`)
+    .then(response => response.json())
+    .then(king => {
+        makeWeddingHeader(king)
+        makeWeddingFooter(king)
+        makeLordlessList(king)
+    })
+    .catch(error => console.log(error))
+
+function makeWeddingHeader(king) {
+    let kingH1 = document.createElement("h1")
+    kingH1.innerText = `King ${king.name} has arrived at the wedding.`
+    theKingDiv.appendChild(kingH1)
+}
+
+function makeWeddingFooter(king) {
+    let deadH1 = document.createElement("h1")
+    deadH1.innerText = `King ${king.name} has been killed.`
+    deadDiv.appendChild(deadH1)
+}
+
+function makeLordlessList(king) {
+    king.houses.map(house => {
+        let houseLI = document.createElement("li")
+        houseLI.innerText = house.name
+        lordlessUL.appendChild(houseLI)
+    })
+}
